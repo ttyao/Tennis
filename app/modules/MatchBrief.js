44539e82663b79eacd66b81d6732ed6125beec40
Henry Yao <henry.yao@airbnb.com>
2016-02-05 15:41:58 -0800

    head 2 head

--- app/modules/MatchBrief.js
+++ app/modules/MatchBrief.js
@@ -2,17 +2,26 @@ import React from 'react';
 import PlayerName from './PlayerName';
 import Timestamp from 'react-timestamp';
 var ReactFireMixin = require('reactfire');
+var TimerMixin = require('react-timer-mixin');
+
 
 var MatchBrief = React.createClass({
   propTypes: {
     matchId: React.PropTypes.string,
-    onChange: React.PropTypes.func,
+    onAfterLoad: React.PropTypes.func,
+    visible: React.PropTypes.bool
   },
 
   getInitialState () {
     return {};
   },
-  mixins: [ReactFireMixin],
+  getDefaultProps () {
+    return {
+      visible: true
+    };
+  },
+  mixins: [ReactFireMixin, TimerMixin],
+
   componentWillMount () {
     var ref = new Firebase("https://blistering-torch-8342.firebaseio.com/web/data/matches/"+this.props.matchId);
     this.bindAsObject(ref, "match");
@@ -37,34 +46,41 @@ var MatchBrief = React.createClass({
   },
   render() {
     if (this.state.match) {
-      var date = new Date(this.state.match.matchTime);
-      var winSetNum = this.getWinSetNum();
-      return (
-        <div className="matchBriefBody">
-          <div>
-            <table>
-              <tbody><tr>
-                <td>
-                  <PlayerName winSetNum={winSetNum} key={this.state.match.players.player1} playerId={this.state.match.players.player1} />
-                  <PlayerName winSetNum={winSetNum} key={this.state.match.players.player3} playerId={this.state.match.players.player3} />
-                </td>
-                <td>VS</td>
-                <td>
-                  <PlayerName winSetNum={-winSetNum} key={this.state.match.players.player2} playerId={this.state.match.players.player2} />
-                  <PlayerName winSetNum={-winSetNum} key={this.state.match.players.player4} playerId={this.state.match.players.player4} />
-                </td>
-              </tr></tbody>
-            </table>
+      if (this.props.onAfterLoad) {
+        this.setTimeout(function() { this.props.onAfterLoad(this.state.match['.key'], this.state.match.players);}, 0);
+      }
+      if (this.props.visible) {
+        var date = new Date(this.state.match.matchTime);
+        var winSetNum = this.getWinSetNum();
+        return (
+          <div className="matchBriefBody">
+            <div>
+              <table>
+                <tbody><tr>
+                  <td>
+                    <PlayerName winSetNum={winSetNum} key={this.state.match.players.player1} playerId={this.state.match.players.player1} />
+                    <PlayerName winSetNum={winSetNum} key={this.state.match.players.player3} playerId={this.state.match.players.player3} />
+                  </td>
+                  <td>VS</td>
+                  <td>
+                    <PlayerName winSetNum={-winSetNum} key={this.state.match.players.player2} playerId={this.state.match.players.player2} />
+                    <PlayerName winSetNum={-winSetNum} key={this.state.match.players.player4} playerId={this.state.match.players.player4} />
+                  </td>
+                </tr></tbody>
+              </table>
+            </div>
+            <div className="dividerDiv">
+              {this.getScore()}
+            </div>
+            <div>
+            {this.state.match.message}
+            </div>
+            <Timestamp time={date.toISOString()} />
           </div>
-          <div>
-            {this.getScore()}
-          </div>
-          <Timestamp time={date.toISOString()} />
-        </div>
-      );
-    } else {
-      return (<div/>);
+        );
+      }
     }
+    return (<div/>);
   }
 });
 
--- app/modules/PlayerSelect.js
+++ app/modules/PlayerSelect.js
@@ -4,25 +4,15 @@ import Select from 'react-select';
 var PlayerSelect = React.createClass({
   displayName: 'PlayerSelect',
   propTypes: {
-    label: React.PropTypes.string,
-    value: React.PropTypes.array,
+    player: React.PropTypes.string,
     onChange: React.PropTypes.func,
   },
   getInitialState () {
-    var ref = new Firebase("https://blistering-torch-8342.firebaseio.com");
-    var authData = ref.getAuth();
-    return { player1: authData["uid"], player2: null};
+    return { player: this.props.player};
   },
-  handleSelectChange1 (value, values) {
-    this.setState({ player1: value });
-    this.props.onChange("player1", value);
-  },
-  handleSelectChange2 (value, values) {
-    this.setState({ player2: value });
-    this.props.onChange("player2", value);
-  },
-  toggleDisabled (e) {
-    this.setState({ 'disabled': e.target.checked });
+  handleSelectChange (value, values) {
+    this.setState({ player: value });
+    this.props.onChange(value);
   },
 
   loadOptions(input, callback) {
@@ -48,17 +38,7 @@ var PlayerSelect = React.createClass({
         <tbody><tr>
           <td className="playersection">
             <span className="section">
-              <Select multi value={this.state.player1} placeholder="Select player(s)" onChange={this.handleSelectChange1} asyncOptions={this.loadOptions} />
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td className="divider"> VS </td>
-        </tr>
-        <tr>
-          <td className="playersection">
-            <span className="section">
-              <Select multi value={this.state.player2} placeholder="Select player(s)" asyncOptions={this.loadOptions} onChange={this.handleSelectChange2} />
+              <Select value={this.state.player} placeholder="Select player" onChange={this.handleSelectChange} asyncOptions={this.loadOptions} />
             </span>
           </td>
         </tr></tbody>
